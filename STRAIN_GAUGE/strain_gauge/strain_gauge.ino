599d54c946392d080eccf36525334176c4ef8b66
Siddhu Jaykay <siddhujaykay2@gmail.com>
2024-06-12 15:27:22 +0530

    test : strain gauge sensitivity

--- STRAIN_GAUGE/strain_gauge/strain_gauge.ino
+++ STRAIN_GAUGE/strain_gauge/strain_gauge.ino
@@ -5,10 +5,21 @@ void setup() {
 void loop() {
   float time = micros()/1e6;
 
-  int sensorValue = analogRead(A0);
+  int sensorValue = 1024 - analogRead(A0) - 2.5;
+  double volts = ((float)sensorValue / 1024) * 5;
+  double mStrain = volts*1000/2;
   delay(100);
-
   Serial.print(time);
   Serial.print(", ");
-  Serial.println(sensorValue);
+  Serial.print(sensorValue);
+  Serial.print(", ");
+  Serial.print(volts);
+  Serial.print(", ");
+  Serial.println(mStrain);
+
+
+
+  // x / 5 = sens_count / 1024
+
+  // x = (sens_count / 1024) * 5
 }
